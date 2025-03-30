fed697f94926bc074f0c18a1e8fbac161c473dc3
kondakings <161365579+kondakings@users.noreply.github.com>
2025-03-30 12:14:42 +0100

    Add files via upload
    Files

---
+++ contactButtons.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const contactButtonsHTML = `
+//         <div class="contact-buttons">
+//             <a href="https://wa.me/123456789" target="_blank" class="whatsapp-button">
+//                 <img src="./whatsapp-icon.png" alt="WhatsApp" class="whatsapp-icon" />
+//             </a>
+//             <a href="tel:123456789" class="phone-button">
+//                 <img src="./phone-icon.png" alt="Phone" class="phone-icon" />
+//             </a>
+//         </div>
+//     `;
+//     document.body.insertAdjacentHTML("beforeend", contactButtonsHTML);
+// });
